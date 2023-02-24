1e2c172b356737c52024920e01dbf61b72cb9b8b
AlexandreSCorreia <alex.sol.correia@gmail.com>
2023-02-24 15:24:49 -0300

    Adicionando projeto base do banco de dados

---
+++ Db/create-database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE LZLtech;
+GO
+Use LZLtech;
+GO
+CREATE TABLE Produtos
+( 
+  Id          smallint identity(1,1)
+, Descricao     VARCHAR(255) NOT NULL
+, Quantidade    INT NOT NULL
+, CONSTRAINT pkProdutos PRIMARY KEY (Id)
+);
+GO
