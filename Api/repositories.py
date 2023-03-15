b38dce757c701f95a9e238db1c05d4f53504385e
Elvis Liberato <liberatov13@gmail.com>
2023-03-14 23:11:13 -0300

    Inserindo tipagem aos models

--- Api/repositories.py
+++ Api/repositories.py
@@ -1,30 +1,32 @@
 from app import db, joinedload
 from interfaces import IRepository
 from models import Produto, TipoProduto
+import logging as log
 
 class RepositoryBase(IRepository):
     def __init__(self, classe):
         self.classe = classe
 
     def find(self, id):
-        item = self.classe.query.get(id)            
-        return item         
+        item = self.classe.query.get(id)
+        return item
 
     def findAll(self):
-        list = self.classe.query.order_by(self.classe.id).all() 
+        list = self.classe.query.order_by(self.classe.id).all()
         return list
 
     def create(self, obj):
         try:
-            db.session.add(obj) 
+            db.session.add(obj)
             db.session.commit()
             return True
-        except:
+        except Exception as ex:
+            log.error(ex)
             db.session.rollback()
             return False
 
     def update(self, id, obj):
-        item = self.find(id)  
+        item = self.find(id)
         if item is not None:
             for key, value in obj.__dict__.items():
                 if key != '_sa_instance_state':
@@ -34,16 +36,17 @@ class RepositoryBase(IRepository):
         return False
 
     def destroy(self, id):
-        item = self.find(id) 
-        if item is not None:        
+        item = self.find(id)
+        if item is not None:
             try:
                 db.session.delete(item)
                 db.session.commit()
                 return True
-            except:
+            except Exception as ex:
+                log.error(ex)
                 return False
         return False
-    
+
 class ProdutoRepository(RepositoryBase):
     def __init__(self):
         super().__init__(Produto)
@@ -52,9 +55,18 @@ class ProdutoRepository(RepositoryBase):
         list = db.session.query(Produto).options(
             joinedload('unidade_medida'),
             joinedload('tipo_produto')
-        ).all()    
+        ).all()
         return list
 
 class TipoProdutoRepository(RepositoryBase):
     def __init__(self):
         super().__init__(TipoProduto)
+
+
+    def findByDescricao(descricao: str):
+        try:
+            tipo_produto: TipoProduto = db.session.query(TipoProduto).filter_by(descricao=descricao)
+            return tipo_produto
+        except Exception as ex:
+            log.error(ex)
+            return None
